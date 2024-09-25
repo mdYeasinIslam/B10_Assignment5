189afd895587ba32250f1d9bfee38bb3dd29fb29
mdYeasinIslam <hyeasinislam@gmail.com>
2024-09-25 11:34:39 +0600

    feni donation area complete

--- js/main.js
+++ js/main.js
@@ -32,5 +32,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     // card-2
-    
+    document.getElementById('donate-feni-button').addEventListener('click', function (e) {
+        e.preventDefault()
+        console.log('clicked')
+        const inputValue = getInputFieldByID('second-input-field')
+        const elementValue = getElement('donate-amount-second')
+        const mainAmount = getElement('main-amount')
+        if (mainAmount > inputValue) {
+            if (inputValue > 0 && elementValue >= 0) {
+                document.getElementById('donate-amount-second').innerText = elementValue + inputValue
+                document.getElementById('main-amount').innerText = mainAmount - inputValue;
+                my_modal_5.showModal()
+
+                // update history section
+                const makeHistorySect = document.getElementById('history-section')
+                const donateArea = document.getElementById('donate-area-feni').innerText
+                console.log(donateArea)
+                const div = document.createElement('div')
+                div.innerHTML = ` 
+                <h1>${inputValue} Taka is Donated for ${donateArea}</h1>
+                `
+                div.classList.add('history-style')
+                makeHistorySect.appendChild(div)
+            }
+            else {
+                alert('please donate more that 1 bdt')
+            }
+        }
+        else {
+            alert('sorry , you donnot have sufficient balance')
+
+        }
+    })
 })
